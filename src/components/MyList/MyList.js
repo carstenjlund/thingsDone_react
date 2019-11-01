cba142a135a5ead850c23e19aabd1a5b862725cd
clurts <clu@rts.dk>
2019-11-01 07:58:06 +0100

    optimize mylist

--- src/components/MyList/MyList.js
+++ src/components/MyList/MyList.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 export default function MyList({ thingsDone }) {
-  const thingsList = thingsDone.map((current) => {
-    return (<li key={current.id}>{current.thing}</li>)
-  });
   return (
     <ul>
-      {thingsList}
+      {thingsDone.map(current => (
+        <li key={current.id}>{current.thing}</li>
+      ))}
     </ul>
   )
 }
