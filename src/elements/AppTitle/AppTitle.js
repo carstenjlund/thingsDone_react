befdbb68fb122ffb4fae9444aabefe205d6e9e05
clurts <clu@rts.dk>
2019-11-02 09:33:20 +0100

    Add AppTitle

---
+++ src/elements/AppTitle/AppTitle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function AppTitle() {
+    return (
+        <React.Fragment>
+            <h1>Things I have done today</h1>
+        </React.Fragment>
+    )
+}
